78f4132b413d0909dd22542052202ee673b3fc22
Doug Shannon <doug@tensure.io>
2020-07-27 09:36:20 -0400

    removed hook code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "./App.css";
 import Movie from "./movie/movie";
 
 function App() {
-  const [movies, setMovies] = useState([]);
   const mdbUrl = "https://api.themoviedb.org/3/";
   const posterUrl = "http://image.tmdb.org/t/p/w500/";
   const mdbApi = "api_key=3da7e91a9c2125919c37b777de884c03";
   const mdbSearch = `${mdbUrl}search/movie/`;
 
-  useEffect(() => {
-    searchMovie({ title: "the matrix" }).then((results) => {
-      setMovies(results.results);
-    });
-  }, []);
 
-  useEffect(() => {
-    if (movies.length > 0) {
-      console.log("2nd use effect: " + movies[0].title);
-    }
-  }, [movies]);
+
+
 
   const searchMovie = ({title}) => {
     // www.themoviedb.org/search/movie?api_key=asdlkjasjdalsjkd&query=the%20matrix
@@ -32,7 +23,7 @@ function App() {
 
   const textChanged = (e) => {
     searchMovie({ title: e.target.value }).then((results) => {
-      setMovies(results.results);
+      // somehow set movies here when user types
     });
   };
 
