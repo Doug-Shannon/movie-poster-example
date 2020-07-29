24317a96651757be0e00fe4a1765b27fdc4278b1
Doug Shannon <doug@tensure.io>
2020-07-29 10:06:34 -0400

    added movie description on click

--- src/App.js
+++ src/App.js
@@ -1,34 +1,35 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import "./App.css";
 import Movie from "./movie/movie";
 
+const mdbUrl = "https://api.themoviedb.org/3/";
+const posterUrl = "http://image.tmdb.org/t/p/w500/";
+const mdbApi = "api_key=3da7e91a9c2125919c37b777de884c03";
+const mdbSearch = `${mdbUrl}search/movie/`;
+
 function App() {
   const [movies, setMovies] = useState([]);
-  const mdbUrl = "https://api.themoviedb.org/3/";
-  const posterUrl = "http://image.tmdb.org/t/p/w500/";
-  const mdbApi = "api_key=3da7e91a9c2125919c37b777de884c03";
-  const mdbSearch = `${mdbUrl}search/movie/`;
 
-  useEffect(() => {
-    searchMovie({ title: "the matrix" }).then((results) => {
-      setMovies(results.results);
-    });
-  }, []);
-
-  useEffect(() => {
-    if (movies.length > 0) {
-      console.log("2nd use effect: " + movies[0].title);
-    }
-  }, [movies]);
-
-  const searchMovie = ({title}) => {
+  const searchMovie = useCallback(({ title }) => {
     // www.themoviedb.org/search/movie?api_key=asdlkjasjdalsjkd&query=the%20matrix
     if (title.length > 0) {
       return fetch(`${mdbSearch}?${mdbApi}&query=${encodeURIComponent(title)}`)
         .then((resp) => resp.json())
         .catch((error) => console.error(error));
     }
-  };
+  }, []);
+
+  useEffect(() => {
+    searchMovie({ title: "the matrix" }).then((results) => {
+      setMovies(results.results);
+    });
+  }, [setMovies, searchMovie]);
+
+  useEffect(() => {
+    if (movies.length > 0) {
+      console.log("2nd use effect: " + movies[0].title);
+    }
+  }, [movies]);
 
   const textChanged = (e) => {
     searchMovie({ title: e.target.value }).then((results) => {
@@ -39,17 +40,22 @@ function App() {
   return (
     <div>
       <input onChange={textChanged} />
-      <div style={{ display: "flex", flexFlow: "row wrap" }}>
+      <div className="movieList">
         {movies.map((movie) => {
           if (movie.poster_path != null) {
             return (
               <Movie
                 key={movie.id}
                 name={movie.title}
+                description={movie.overview}
+                onClick={(e) => {
+                  console.log("clicked");
+                }}
                 posterpath={`${posterUrl}${movie.poster_path}`}
               ></Movie>
             );
           }
+          return null;
         })}
       </div>
     </div>
--- src/movie/movie.js
+++ src/movie/movie.js
@@ -1,18 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
+// import { useTransition, animated } from "react-spring";
 import "./movie.css";
 
-function Movie({posterpath, name}) {
+function Movie({ posterpath, name, description }) {
+  const [selected, setSelected] = useState(false);
+  // const transitions = useTransition(selected, null, {
+  //   from: { opacity: 0 },
+  //   enter: { opacity: 1 },
+  //   leave: { opacity: 0 },
+  // });
   //   return <p>{props.name + " " + props.posterpath}</p>;
+
   return (
     <div
-      style={{
-        width: "500px",
-        height: "750px",
-        border: "1px solid black",
-        backgroundImage: `url(${posterpath})`,
-        backgroundSize: "cover"
+      onClick={(e) => {
+        setSelected(!selected);
       }}
-    ></div>
+      className="poster"
+      style={
+        selected
+          ? {}
+          : {
+              backgroundImage: `url(${posterpath})`,
+              backgroundSize: "cover",
+            }
+      }
+    >
+      {selected ? (
+        <div>
+          <h1 className="title">{name}</h1>
+          <div>
+            <p>{description}</p>
+          </div>
+        </div>
+      ) : (
+        ""
+      )}
+      {/* {transitions.map(({ item, key, props }) =>
+        item ? (
+          <animated.div style={props}>
+            <h1 className="title">{name}</h1>
+            <div>
+              <p>{description}</p>
+            </div>
+          </animated.div>
+        ) : (
+          ""
+        )
+      )} */}
+    </div>
   );
 }
 
